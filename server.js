8b9d57fb69f7224ba5c84b58b68a650200e5e555
Yagya Jha <74773203+Yagya-Jha@users.noreply.github.com>
2023-06-24 16:34:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const io = require("socket.io")(server, {cors: {origin: '*'}});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on("connection", (socket) => {
+    socket.on("message", (message, userName) => {
+        io.emit("createMessage", message, userName);
+    });
+});
+
+server.listen(3030);
